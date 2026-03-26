9e7e6253a2566dd16f1c333267e3fef0e96a3a67
Asura <benaichhamed@gmail.com>
2026-03-26 16:49:17 +0100

    modifying telegran.php

--- telegram.php
+++ telegram.php
@@ -9,10 +9,16 @@ $data = json_decode(file_get_contents('php://input'), true);
 $text = $data['text'];
 
 $url = "https://api.telegram.org/bot$token/sendMessage";
-$response = file_get_contents($url . "?" . http_build_query([
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([
     'chat_id' => $chat_id,
     'text' => $text
 ]));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
 
 echo $response;
 ?>
